37df2752aacf844f37decedbf9c373e70cb3df5c
mohanprasaths <mohanprasath@live.com>
2018-02-21 21:38:12 +0530

    Properties getter and setter

--- Classmethods.py
+++ Classmethods.py
@@ -1,36 +1,52 @@
 class Superhero:
     avengerCount = 0
     superHeroCount = 0
-    def __init__(self,name,power,isAvenger):
+    def __init__(self,name,power,isAvenger,topClass):
         self.name = name
         self.power = power
-        self.superHeroCounter()
+        self._superHeroCounter()
         if isAvenger:
-            Superhero.avengerCountAdd()
+            Superhero._avengerCountAdd()
+        self.topClass = topClass
 
     @staticmethod
-    def avengerCountAdd():
+    def _avengerCountAdd():
         Superhero.avengerCount += 1
         return Superhero.avengerCount
 
 
     @staticmethod
-    def getAvengerGet():
+    def _getAvengerGet():
         return Superhero.avengerCount
 
     #use classMethod if class object is needed
     @classmethod
-    def superHeroCounter(cls):
+    def _superHeroCounter(cls):
         cls.superHeroCount += 1
 
     @classmethod
-    def getSuperHeroCount(cls):
+    def _getSuperHeroCount(cls):
         return cls.superHeroCount
 
+    @property
+    def topClass(self):
+        return self._topClass
 
-ironman = Superhero("tony" , 99 , True)
-captain = Superhero("steve", 98 , True)
-warlock = Superhero("adam" , 89 , False)
+    @topClass.setter
+    def topClass(self,isTopClass):
+        if isTopClass and self.power > 90:
+            self._topClass = True
+        else:
+            raise Exception("Not enough power for {name}".format(name= self.name))
 
-print(Superhero.getAvengerGet())
-print(Superhero.getSuperHeroCount())
+try:
+    ironman = Superhero("tony" , 99 , True,True)
+    captain = Superhero("steve", 98 , True,True)
+    blackwidow = Superhero('Natasa',65 , True, True)
+    warlock = Superhero("adam" , 89 , False,True)
+    blackwidow = Superhero('Natasa',65 , True, True)
+except Exception as e:
+    print(e)
+
+print(Superhero._getAvengerGet())
+print(Superhero._getSuperHeroCount())
